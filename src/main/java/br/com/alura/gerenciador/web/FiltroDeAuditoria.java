07d222df577de6a782863b5e9d0dc1c6364ace45
fjunior <fabio.simplis@gmail.com>
2017-08-25 21:47:57 -0300

    Mantendo informações relativas ao usuário com cookies

--- src/main/java/br/com/alura/gerenciador/web/FiltroDeAuditoria.java
+++ src/main/java/br/com/alura/gerenciador/web/FiltroDeAuditoria.java
@@ -9,6 +9,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 @WebFilter(urlPatterns="/*")
@@ -24,10 +25,35 @@ public class FiltroDeAuditoria implements Filter{
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
 		
-		HttpServletRequest req =(HttpServletRequest) request;
+		HttpServletRequest req =(HttpServletRequest) request; 
 		String filter = req.getRequestURI();// pega a uri de acesso 
-		System.out.println("Acessando: "+ filter);
+		Cookie cookie = getUsuario(req);
+		String usuario = "<deslogado>";
+		
+		if(cookie!=null)
+			usuario = cookie.getValue();
+		
+		System.out.println("Usuario: " + usuario + "Acessando: "+ filter);
+		
 		chain.doFilter(request, response); //continua o processo.
+	}
+
+	private Cookie getUsuario(HttpServletRequest req) {
+		
+		Cookie[] cookies = req.getCookies();
+		
+		if (cookies == null)// verifica a existencia de um cookie
+	        return null;
+		
+		for (Cookie cookie : cookies) {
+			
+			
+			if (cookie.getName().equals("1")) {
+				return cookie;
+			}
+			
+		}
+		return null;
 	}
 
 	@Override
